16ea5d8d683f954a9b4f2cc316590db47e1f58e7
Kunologist <19852747+Gennadiyev@users.noreply.github.com>
2018-08-28 14:03:55 +0800

    init.lua with comments

---
+++ code/init.lua
@@ -0,0 +1,19 @@
+-- init.lua contains the basic information of the app.
+
+appname="Kun Calc" -- The caption of the app.
+appver="2.7" -- This means the version is 2, and has a total achievement of 7.
+appcode="24" -- Just the version number. Will be updated on release.
+appsdk="18" -- Do not modify.
+path_pattern="" -- Do not modify.
+packagename="com.wordpress.kunologist" -- Do not modify.
+theme="Theme_DeviceDefault_NoActionBar_Fullscreen" -- Specifies the theme. Do not modify.
+app_key="" -- Do not modify.
+app_channel="" -- Do not modify.
+developer="Kunologist" -- Why not add your name here? Use semicolons and spaces to add more names beside me!
+description="A Simple calculator if you believe." -- I do not believe.
+debugmode=true -- Do not modify.
+user_permission={
+  "WRITE_EXTERNAL_STORAGE",
+  "WRITE_SETTINGS"
+}
+-- I want to require no permission actually.
