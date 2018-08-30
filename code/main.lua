deea8b391867be68679e519bf980379c2cad9819
Kunologist <19852747+Gennadiyev@users.noreply.github.com>
2018-08-30 11:38:57 +0800

    3.10
    - Added 2 new achievements to resolve #9, #11.
    - Created new printing methods, styling the output differently.
    - Indent fixing and space adding.

--- code/main.lua
+++ code/main.lua
@@ -9,7 +9,7 @@ activity.setTheme(android.R.style.Theme_Material_Light)
 activity.setContentView(loadlayout("layout"))
 
 WHAT_KUN_WANTS = 960
-ACHIEVEMENT_SUM = 8
+ACHIEVEMENT_SUM = 10
 
 found = 0
 mode = 0
@@ -22,7 +22,7 @@ cancelling = 0
 kunIsBack = 0
 calcAndCalc = 0
 knowingWhatKunWants = 0
-
+errorsCaught = 0
 theEnd = 0
 
 function printMsgAdvanced(textToPrint, changeColor, changeSize)
@@ -32,10 +32,23 @@ function printMsgAdvanced(textToPrint, changeColor, changeSize)
 	printer.textSize = changeSize
 end
 
-function printMsg(textToPrint)
+function printMsgNormal(textToPrint)
 	printer.text = textToPrint.."\n\n"..printer.text
+	printer.textColor = "#4a4a98"
+	printer.textSize = "18"
 end
 
+function printMsgAchievement(textToPrint)
+	printer.text = textToPrint.."\n\n"..printer.text
+	printer.textColor = "#d89a4b"
+	printer.textSize = "18"
+end
+
+function printMsgError(textToPrint)
+	printer.text = textToPrint.."\n\n"..printer.text
+	printer.textColor = "#ff0000"
+	printer.textSize = "18"
+end
 calc.onClick=function(calculate)
 	--the main part,because this is really a calculator
 	if mode==0 then
@@ -57,6 +70,7 @@ calc.onClick=function(calculate)
 	--0
 	if mode == 3 and adder2.text == "0" and error_catch.checked then
 		achievementCounter = achievementCounter + 1
+		if errorsCaught > -1 then errorsCaught = errorsCaught + 1 end
 	end
 	--1
 	if notChanging > -1 and mode == 0 and adder1.text == "20" and adder2.text == "10" then
@@ -100,28 +114,40 @@ calc.onClick=function(calculate)
 	if knowingWhatKunWants > -1 and mode == 3 and tonumber(adder1.text)/tonumber(adder2.text) == WHAT_KUN_WANTS then
 		knowingWhatKunWants = knowingWhatKunWants + 1
 	end
-
+	
 
 	--Thens
 
+	
+	if result.text == "340.0" then
+		printMsgNormal("有效数字很重要喔！但是没想到吧，这里也有成就！恭喜！")
+		found = found + 1
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+	end
 	--0
 	if achievementCounter > 0 then
-		printer"ERROR: DIVIDED BY 0"
-		printMsg("隐藏成就计数器："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		printMsgNormal("ERROR: DIVIDED BY 0")
+		printMsgNormal("隐藏成就计数器："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		achievementCounter = 0
+	end
+	if errorsCaught == 8 then
+		printMsgNormal("ERROR: DIVIDED BY 0 PLUS YOU'VE EARNED AN ACHIEVEMENT.")
+		found = found + 1
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		errorsCaught = -1
 	end
 	--1
 	if notChanging == 1 then
-		printMsg("您就不能换一个数据嘛！我生气了！我要把答案搞成错误的！")
+		printMsgNormal("您就不能换一个数据嘛！我生气了！我要把答案搞成错误的！")
 		result.text=tostring(WHAT_KUN_WANTS)
 		found = found + 1
-		printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		notChanging = -1
 	end
 	--3
 	if fortyTwo == 2 then
 		found = found + 1
-		printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		adder1.text="SIX"
 		运算符.text="MULTIPLIED BY"
 		adder2.text="NINE"
@@ -129,24 +155,24 @@ calc.onClick=function(calculate)
 	end
 	--6
 	if calcAndCalc == 40 then
-		printMsg("这是您的第40次计算。那么，作为奖励——")
+		printMsgNormal("这是您的第40次计算。那么，作为奖励——")
 		found=found+1
-		printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		calcAndCalc = -1 
 	end
 	--7
 	if knowingWhatKunWants > 0 and knowingWhatKunWants < 11 then
-		printMsg("说，您调戏这个计算器多久了？那么熟练？")
+		printMsgNormal("说，您调戏这个计算器多久了？那么熟练？")
 		knowingWhatKunWants = knowingWhatKunWants + 1
 	end
 	if knowingWhatKunWants > 10 and knowingWhatKunWants < 20 then
-		printMsg("不给不给不给"..string.rep("！",knowingWhatKunWants-10))
+		printMsgNormal("不给不给不给"..string.rep("！",knowingWhatKunWants-10))
 		knowingWhatKunWants = knowingWhatKunWants + 1
 	end
 	if knowingWhatKunWants == 20 then
-		found=found+1
-		printMsg("好的，好的。你赢了。不就是成就嘛。给你加一个。")
-		printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		found = found + 1
+		printMsgNormal("好的，好的。你赢了。不就是成就嘛。给你加一个。")
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		knowingWhatKunWants = -1
 	end
 
@@ -166,7 +192,7 @@ config.onClick=function(changeMode)
 
 	mode = (mode + 1) % 4
 	运算符.text = string.sub("+-*/",mode+1,mode+1)
-	printMsg("现在运算符变成了 "..string.sub("+-*/",mode+1,mode+1))
+	printMsgNormal("现在运算符变成了 "..string.sub("+-*/",mode+1,mode+1))
 
 
 	--Ifs
@@ -187,10 +213,10 @@ config.onClick=function(changeMode)
 
 	--2
 	if hittingModes == 20 then
-		found=found+1
-		printMsg("你是真的很无聊诶。")
-		printMsg("……不过不无聊的人怎么会玩计算器呢")
-		printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+		found = found + 1
+		printMsgNormal("你是真的很无聊诶。")
+		printMsgNormal("……不过不无聊的人怎么会玩计算器呢")
+		printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 		hittingModes = -1
 		fortyTwo = fortyTwo + 1
 		result.text = "Answer to the Ultimate Question\nof Life\nthe Universe\nand Everything."
@@ -274,9 +300,9 @@ test.onClick=function(test)
 		--5
 		if kunIsBack == 1 then
 			found = found + 1
-			printMsg("鲲又回来了！")
-			printMsg("不过这个提示也太明显了吧……算了，勉强给您一个。")
-			printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+			printMsgNormal("鲲又回来了！")
+			printMsgNormal("不过这个提示也太明显了吧……算了，勉强给您一个。")
+			printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 			kunIsBack = -1
 		end
 		--coming soon!
@@ -296,12 +322,12 @@ test.onClick=function(test)
 		end
 
 		--easter eggs
-		if appname.text=="鲲的英文名" or appname.text == "鲲的英语名" or appname.text == "鲲的英语名试试？" or appname.text == "鲲的英语名试试" then printMsg("喔，你以为这样就能蒙混过关？好的。不给过！哼。") end
+		if appname.text=="鲲的英文名" or appname.text == "鲲的英语名" or appname.text == "鲲的英语名试试？" or appname.text == "鲲的英语名试试" then printMsgNormal("喔，你以为这样就能蒙混过关？好的。不给过！哼。") end
 		if string.sub(appname.text,1,3)=="Kun" and appname.text~="Kunologist" then
-			printMsg("Uh oh, wrnog speling! Try agian?") 
+			printMsgError("Uh oh, wrnog speling! Try agian?") 
 		end --My ideas! From Richardn
 		if string.sub(appname.text,1,3)=="kum" or string.sub(appname.text,1,3)=="kun" or string.sub(appname.text,1,3) == "Kum" then 
-			printMsg("请首字母大写并检查拼写") 
+			printMsgError("请首字母大写并检查拼写") 
 		end
 		
 	end})
@@ -325,11 +351,11 @@ test.onClick=function(test)
 		--Thens
 		--4
 		if cancelling > -1 and cancelling < 5 then
-			printMsg("哇！居然点了取消！真没想到啊！那……把程序名字设定为鲲的英语名试试"..string.rep("? ",cancelling))
+			printMsgAdvanced("哇！居然点了取消！真没想到啊！那……把程序名字设定为鲲的英语名试试"..string.rep("? ",cancelling),"#db5b6a","23")
 		end
 		if cancelling == 5 then
 			found = found + 1
-			printMsg("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
+			printMsgAchievement("隐藏成就达成："..tostring(found).."/"..tostring(ACHIEVEMENT_SUM).."。")
 			cancelling = -1
 		end
 
